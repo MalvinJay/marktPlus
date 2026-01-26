596191963426a206ff6cc8a9bacf676cc0fede28
George Arthur <Arthurgeorgekojo@gmail.com>
2026-01-26 00:30:19 +0000

    Implement api url routes, and updated depenedencies

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from accounts.views import UserRegistrationView, UserProfileViewSet
+from dashboard.views import DashboardMetricViewSet
+from transactions.views import TransactionViewSet
+from reports.views import ReportViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserRegistrationView, basename='user-registration')
+router.register(r'profiles', UserProfileViewSet, basename='user-profile')
+router.register(r'dashboard/metrics', DashboardMetricViewSet, basename='dashboard-metrics')
+router.register(r'transactions', TransactionViewSet, basename='transaction')
+router.register(r'reports', TransactionViewSet, basename='report')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
+    path('api/auth/token/', TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path('api/auth/token/refresh/', TokenRefreshView.as_view(), name="token_refresh"),
+    path('api-auth/', include('rest_framework.urls'))
 ]
